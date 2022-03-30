a14a37b6ba8b2460847031e9f4f4de9828d763e6
Burak Ocak <burakocak1903@gmail.com>
2022-03-30 10:47:38 +0300

    add canned response data and adapter

--- livechat/src/main/java/com/desk360/livechat/data/model/message/MessageRequest.kt
+++ livechat/src/main/java/com/desk360/livechat/data/model/message/MessageRequest.kt
@@ -1,5 +1,6 @@
 package com.desk360.livechat.data.model.message
 
+import com.desk360.livechat.data.model.cannedresponse.CannedPayloadModel
 import com.google.gson.annotations.SerializedName
 
 data class MessageRequest(
@@ -7,5 +8,7 @@ data class MessageRequest(
     val email: String? = null,
     val message: String? = null,
     val source: String = "Android",
-    @SerializedName("settings") var settings:Map<String,String>? = mapOf()
+    @SerializedName("settings") var settings:Map<String,String>? = mapOf(),
+    @SerializedName("path_id") var pathId:Int? = null,
+    @SerializedName("canned_response_payload") var payload:ArrayList<CannedPayloadModel>? = arrayListOf(),
 )
