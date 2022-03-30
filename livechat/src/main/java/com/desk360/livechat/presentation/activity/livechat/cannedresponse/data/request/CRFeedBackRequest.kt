cd405015e0f6d1f64e72d60d1d02711352249c24
Burak Ocak <burakocak1903@gmail.com>
2022-03-30 10:40:12 +0300

    delete viewholder

--- livechat/src/main/java/com/desk360/livechat/presentation/activity/livechat/cannedresponse/data/request/CRFeedBackRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.desk360.livechat.presentation.activity.livechat.cannedresponse.data.request
-
-import com.google.gson.annotations.SerializedName
-
-data class CRFeedBackRequest(
-    @SerializedName("feedback") val feedBack: Int,
-    @SerializedName("path_id") val pathId:Int?,
-    @SerializedName("canned_response_payload") val payloadData: ArrayList<HashMap<Int,String>>
-)
