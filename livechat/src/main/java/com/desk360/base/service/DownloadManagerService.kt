4323890a834a555db435bdb0feaa4a518b8d4228
Baran Oguz <baranoguz@teknasyon.com>
2024-11-13 19:40:37 +0300

    [feature] Remove storage permissions to comply store policies

--- livechat/src/main/java/com/desk360/base/service/DownloadManagerService.kt
+++ livechat/src/main/java/com/desk360/base/service/DownloadManagerService.kt
@@ -1,33 +1,36 @@
 package com.desk360.base.service
 
 import android.app.DownloadManager
+import android.content.Context
 import android.content.Context.DOWNLOAD_SERVICE
 import android.net.Uri
 import android.os.Environment
 import com.desk360.livechat.manager.Desk360LiveChat
 
 object DownloadManagerService {
-    private var INSTANCE: DownloadManager? = null
-    private val instance: DownloadManager
-        get() {
-            if (INSTANCE == null) {
-                INSTANCE = Desk360LiveChat.getContext()?.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-            }
-            return INSTANCE!!
-        }
+    private val context: Context?
+        get() = Desk360LiveChat.getContext()
 
-    fun start(url: String, fileName: String, description: String): Long {
+    private val downloadManager: DownloadManager? by lazy {
+        Desk360LiveChat.getContext()?.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+    }
+
+    fun start(url: String, fileName: String, description: String): Long? {
+        val context = this.context ?: return null
+        val downloadManager = this.downloadManager ?: return null
+
         val downloadRequest = DownloadManager.Request(Uri.parse(url))
             .setTitle(fileName)
             .setDescription(description)
             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
             .setDestinationInExternalFilesDir(
-                Desk360LiveChat.getContext(),
+                context,
                 Environment.DIRECTORY_DOWNLOADS,
                 fileName
             )
             .setAllowedOverMetered(true) // Mobile Network todo: boyutu büyük olan dosyalar için wifi bağlantısını zorunlu yapmamız gerekebilir.
             .setAllowedOverRoaming(true) // Roaming Network
-        return instance.enqueue(downloadRequest)
+
+        return downloadManager.enqueue(downloadRequest)
     }
 }
